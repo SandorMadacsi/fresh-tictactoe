e9be80de821fb45753729d9d9c5c7b421b665819
Sandor Madacsi <smadacsi@protonmail.com>
2024-11-10 13:09:38 +0000

    initial commit

---
+++ tictactoe.js
@@ -0,0 +1,40 @@
+
+const winConditions = [
+    [
+       [0,1,2],
+       [3,4,5],
+       [6,7,8]
+   ],
+   
+   [
+       [0,3,6],
+       [1,4,7],
+       [2.5,8]
+   ],
+   [
+       [0,4,8],
+       [2,4,6]
+   ]
+
+]
+function Board(){
+
+}
+
+
+function Cell(){
+
+}
+
+function Controller(
+    player1 = "p1",
+    player2 = "p2"
+){
+    const board = Board();
+
+}
+
+
+function boardInterface(){
+
+}
