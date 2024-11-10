5ee6f9c556aafc720e4385445461a4872c6bb0b2
Sandor <smadacsi@protonmail.com>
2024-11-10 22:22:30 +0000

    base skeleton setup up, work on flow of the game

--- tictactoe.js
+++ tictactoe.js
@@ -19,22 +19,64 @@ const winConditions = [
 ]
 function Board(){
 
+    const board = [];
+
+    for(let i = 0; i < 9; i++){
+        board.push(Cell());
+    }
+
+    const getBoard = () => {
+        return board.map((cell) => cell.getValue());
+    }
+
+    const dropToken = (cellPos,playerToken) => {
+        board[cellPos].setValue(playerToken);
+    }
+
+    return{getBoard, dropToken}
 }
 
 
 function Cell(){
 
+    let value  = null;
+
+    const setValue = (player) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return {setValue, getValue};
 }
 
 function Controller(
-    player1 = "p1",
-    player2 = "p2"
-){
+    player1 = Player("player1", "x"),
+    player2 = Player("player2", "o")){
+
     const board = Board();
 
+
+    }
+    
+function Player(name, token){
+    const name = name;
+    const token = token;
+    let score = 0;
+
+    const getName = () => name;
+    const getToken = () => token;
+    const getScore = () => score;
+    const setScore = () => {
+        this.score = score + 1;
+    }
+
+    return{getName, getToken, getScore, setScore}
 }
 
-
-function boardInterface(){
+    
+function boardInterface()
+{
+    // UI / DOM LOGIC GOES HERE
 
 }
